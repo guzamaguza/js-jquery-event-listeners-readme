0c8c61582afa25eedae41234ea4acec628144e67
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2019-11-29 11:49:38 -0800

    Solution. Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,35 @@
 //define functions here
+function getIt(){
+  $('p').on("click", function(){
+      alert('Hey!');
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+     $('img').addClass("tasty");
+     $(".tasty").css("border-color","red"); 
+  });
+}
+
+function pressIt(){
+  $('input').on('keydown', function(key) {
+  if(key.which == 71){
+      alert('s was pressed');
+  }
+  });
+}
+
+function submitIt(){
+    $("form").on("submit", function() {
+  
+    alert('Your form is going to be submitted now.');
+    return;
+ 
+    });
+}
+
+
 
 $(document).ready(function(){
 
